40d6712f2413300f9a3d9c938a9ebf980d7498af
maysamorais <maysa.schlenker@escola.pr.gov.br>
2026-05-07 12:24:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,9 +14,16 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-10-05T00:00:00"); 
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00"); 
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+contadores[0].textContent = calculaTempo (tempos{0});
+
+
 
 function calculaTempo(tempoObjetivo){
 let tempoAtual = new Date();
@@ -32,3 +39,12 @@ let dias = Math.floor(horas / 24);
  
  return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
+
+function atualizaCronometro(){
+    for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+   }
+    
+}
+atualizaCronometro();
+setInterval(atualizaCronometro, 1000);
